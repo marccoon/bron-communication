a7009f7d5315dbb89e489fa46fb4ce353d9ad71a
Evillgranny <tractor2554393@yandex.by>
2021-01-27 19:41:58 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: '660px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1536px'
+     },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
